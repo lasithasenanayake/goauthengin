d6fe9fd4f0394c13e38bb49769f0520ec622e243
lasithasenanayake <lasitha.senanayake@gmail.com>
2014-10-27 16:12:40 +0530

    finish updating

--- testpj/main.go
+++ testpj/main.go
@@ -1,12 +1,70 @@
 package main
 
 import (
+	"code.google.com/p/gorest"
 	"duov6.com/authlib"
 	"duov6.com/term"
+	"encoding/json"
+	"io"
+	"net/http"
 )
 
-func main() {
-	term.Write(term.FgRed, term.BgBlue, term.Read(term.FgBlack, term.BgCyan, "Mother Fucker"), term.ErrorBlink)
+func invoke(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"application/json",
+	)
+	var str string
+	str = `<doctype html>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        Hello World!
+    </body>
+</html>`
+	term.Write("RequestURI "+req.RequestURI, term.Debug)
+	term.Write("Header SecurityToken "+req.Header.Get("SecurityToken"), term.Error)
+	term.Write("RemoteAddr "+req.RemoteAddr, term.Debug)
+	term.Write("PostForm "+req.FormValue("method"), term.Debug)
+	term.Write("PostForm "+req.FormValue("method"), term.Debug)
+	term.Write("req.Method "+req.Method, term.Debug)
+	//key)
+	//
 	a := authlib.Auth{}
-	term.Writec(a.GetAuthCode("sss", "ssss"))
+	switch req.FormValue("method") {
+	case "login":
+		Auth := a.Login(req.FormValue("username"), req.FormValue("password"), req.FormValue("domain"))
+		term.Write("PostForm "+req.FormValue("username"), term.Debug)
+		term.Write("PostForm "+req.FormValue("password"), term.Debug)
+		term.Write("PostForm "+req.FormValue("domain"), term.Debug)
+		b, err := json.Marshal(Auth)
+		if err == nil {
+			//res.Header().Add("", value)
+			io.WriteString(
+				res,
+				string(b),
+			)
+		}
+		return
+
+	default:
+		io.WriteString(
+			res,
+			str,
+		)
+
+	}
+
+}
+
+func main() {
+	gorest.RegisterService(new(authlib.Auth))
+	http.Handle("/", gorest.Handle())
+	//http.HandleFunc("/json", invoke)
+	go http.ListenAndServe(":9000", nil)
+	term.Write(term.Read("What is your name"), term.Information)
+	term.Read("Stop")
+
 }
